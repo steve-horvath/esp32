2094bedd9666cc4e9f772a234cd323b5c2b69680
Steve Horvath <shorvath@evertz.com>
2017-09-02 01:18:36 -0400

    mqtt is mine, mqtt_esp is the example

---
+++ mqtt/mqtt/mqtt.ino
@@ -0,0 +1,134 @@
+#include <PubSubClient.h>
+#include <WiFi.h>
+#include "DHT.h"
+#include <stdlib.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "BELL147";
+const char* password = "64773A7C";
+const char* mqtt_server = "192.168.1.243";
+const int DHTPin = 16;
+#define DHTTYPE DHT22
+
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Temporary variables
+static char celsiusTemp[7];
+static char fahrenheitTemp[7];
+static char humidityTemp[7];
+
+// Client variables 
+char linebuf[80];
+int charcount=0;
+char output;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("ESP32/reconnect", "Module 1 connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  // initialize the DHT sensor
+  dht.begin();
+  
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  while(!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  // attempt to connect to Wifi network:
+  while(WiFi.status() != WL_CONNECTED) {
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+}
+
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+
+
+  client.loop();
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  char msg[5];  
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    Serial.print("Publish message: ");
+    Serial.println(t);
+    Serial.println(h);
+    snprintf(msg, 5, "%f", t);
+    client.publish("temp", msg);
+    snprintf(msg, 5, "%f", h);
+    client.publish("humid", msg);
+  }
+}
