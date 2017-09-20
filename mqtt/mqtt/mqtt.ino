7e43414a5cc933942e6871b65d68d59fc77f54a9
Steve Horvath <shorvath@evertz.com>
2017-09-20 09:39:36 -0400

    fixed issue where client id was getting too long

--- mqtt/mqtt/mqtt.ino
+++ mqtt/mqtt/mqtt.ino
@@ -33,14 +33,17 @@ int value = 0;
 // Server properties
 char *mqttServer = "192.168.1.243";
 int mqttPort = 1883;
-String clientId = "ESP32-1-";
+String clientName = "ESP32-1-";
+String clientId = "";
+
 
 void setup_wifi() {
 
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
-  Serial.print("Wi-Fi Disconnected.  Connecting to ");
+  Serial.print(millis());
+  Serial.print("  Wi-Fi Disconnected.  Connecting to ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
@@ -51,6 +54,7 @@ void setup_wifi() {
   }
 
   Serial.println("");
+  Serial.print(millis());
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
@@ -61,8 +65,10 @@ void mqtt_connect() {
   // Loop until we're reconnected
   while (!mqtt_client.connected()) 
   {
+    Serial.print(millis());
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
+    clientId=clientName;
     randomSeed(micros());
     clientId += String(random(0xffff), HEX);
     Serial.println(clientId);
@@ -70,13 +76,15 @@ void mqtt_connect() {
     mqtt_client.setServer(mqttServer,mqttPort);
     if (mqtt_client.connect(clientId.c_str())) 
     {
-      Serial.println("MQTT connection");// to %d:%i", mqttServer,mqttPort);
+      Serial.print(millis());
+      Serial.println(" MQTT connection");// to %d:%i", mqttServer,mqttPort);
       // Once connected, publish an announcement...
       mqtt_client.publish("ESP32/reconnect", "Module 1 connected");
     } 
     else 
     {
-      Serial.print("failed, rc=");
+      Serial.print(millis());
+      Serial.print(" failed, rc=");
       Serial.print(mqtt_client.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
@@ -101,7 +109,8 @@ void setup() {
   // We start by connecting to a WiFi network
   Serial.println();
   Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print(millis());
+  Serial.print("  Connecting to ");
   Serial.println(ssid);
   
   WiFi.begin(ssid, password);
@@ -110,10 +119,12 @@ void setup() {
   while(WiFi.status() != WL_CONNECTED) {
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     delay(500);
-    Serial.print(".");
+    Serial.print(millis());
+    Serial.println(" .");
   }
   Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.print(millis());
+  Serial.println("  WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   
@@ -125,10 +136,12 @@ void mqtt_publish(int temp, int humid)
 //    Serial.println("Temp is reading as %d",temp);
 //    Serial.println("Humid is reading as %d",humid);
     snprintf(msg, 5, "%d", temp);
-    Serial.print("Temp Messge:  ");
+    Serial.print(millis());
+    Serial.print("  Temp Messge:  ");
     Serial.println(msg);
     mqtt_client.publish("temp", msg);
     snprintf(msg, 5, "%d", humid);
+    Serial.print(millis());
     Serial.print("Humid Messge:  ");
     Serial.println(msg);
     mqtt_client.publish("humid", msg);
@@ -143,11 +156,11 @@ void loop() {
   if (!mqtt_client.connected()) {
     mqtt_connect();
   }
-
   float t = dht.readTemperature();
   float h = dht.readHumidity();
   mqtt_publish(t, h);
-  Serial.println("starting delay");
+  Serial.print(millis());
+  Serial.println("  starting delay");
   delay(30000);
   Serial.println("end delay");
 }
